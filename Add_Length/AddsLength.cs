4ff4b02ef9739b70d6d58d834d5b803a74e4b52d
Дима Ларионов <dima_larionov_000@mail.ru>
2023-01-03 14:19:10 +0300

    Добавьте файлы проекта.

---
+++ Add_Length/AddsLength.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Length;
+
+public class AddsLength
+{
+    public static string[] AddLength(string str)
+    {
+        string[] words = str.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = words[i] + ' ' + words[i].Length;
+        }
+        return words;
+    }
+}
---
+++ Add_Length/Program.cs
@@ -0,0 +1,7 @@
+﻿using Add_Length;
+
+var result = AddsLength.AddLength("you will win");
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
